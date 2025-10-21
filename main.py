25ab7def1ba993baa30343b67af2e71e9f8eca2f
Jonathas Pereira <pjonathas972@gmail.com>
2025-10-21 16:46:48 -0300

    Mexendo com DBeaver

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from sqlmodel import Field, SQLModel
+from typing import Optional
+
+from sqlmodel import Field, Session, SQLModel, create_engine
 
 
 class Hero(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: Optional [int] = Field(default=None, primary_key=True)
     name: str
     secret_name: str
-    age: int | None = None
+    age: Optional[int] = None
+
+engine = create_engine("sqlite:///database.db", echo=True)
+
+SQLModel.metadata.create_all(engine)
+
+hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+hero_2 = Hero(name="Spiuder-Boy", secret_name="Pedro Parqueador")
+hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+
+with Session(engine) as session:
+    session.add(hero_1)
+    session.add(hero_2)
+    session.add(hero_3)
+    session.commit()
